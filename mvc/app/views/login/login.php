0634c49bfaac340911a6a8317320fe0aa5d5b640
SavRares <43829110+SavRares@users.noreply.github.com>
2020-06-11 02:42:38 +0300

    Update login.php

--- mvc/app/views/login/login.php
+++ mvc/app/views/login/login.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <link rel="stylesheet" type="text/css" href="<?php echo URL ?>/public/css/login.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo URL ?>/public/css/navbar.css">
 
-    <title>Login</title>
-  </head>
-  <body>
+<head>
+  <link rel="stylesheet" type="text/css" href="<?php echo URL ?>/public/css/login.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo URL ?>/public/css/navbar.css">
+
+  <title>Login</title>
+</head>
+
+<body>
   <nav>
     <img class="logoimage" src="<?php echo URL ?>/public/assets/logo.png" alt="logo" /></a>
     <div class="title_container">
       <h1>Plant Organiser SSM</h1>
     </div>
   </nav>
-    <div class="login">
-      <img src="<?php echo URL ?>/public/assets/violete.jpg" alt="violete.jpg" class="avatar" />
-      <h1>Login</h1>
-      <form action="controllers/login.php" method="Post">
-        <p>Username</p>
-        <input type="text" placeholder="Enter Username" name="username"/>
-        <p>Password</p>
-        <input type="password" placeholder="Enter Password" name="password"/>
-        <input type="submit" value="Login" />
-        <a id="acc_create" href="<?php echo URL?>/public/?url=register">Create an account!</a>
-      </form>
-    </div>
-  </body>
+  <div class="login">
+    <img src="<?php echo URL ?>/public/assets/violete.jpg" alt="violete.jpg" class="avatar" />
+    <h1>Login</h1>
+    <form action="<?php echo URL ?>/public/login/checkValidity" method="Post">
+      <p>Username</p>
+      <input type="text" placeholder="Enter Username" name="username" />
+      <p>Password</p>
+      <input type="password" placeholder="Enter Password" name="password" />
+      <input type="submit" value="Login" />
+      <a id="acc_create" href="<?php echo URL ?>/public/?url=register">Create an account!</a>
+    </form>
+  </div>
+</body>
+
 </html>
