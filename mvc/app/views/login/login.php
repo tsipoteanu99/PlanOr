03dacc4ab3203bcf74ba85103de2dc22ecec88ea
SavRares <43829110+SavRares@users.noreply.github.com>
2020-06-20 02:41:16 +0300

    login

--- mvc/app/views/login/login.php
+++ mvc/app/views/login/login.php
@@ -18,15 +18,58 @@
   <div class="login">
     <img src="<?php echo URL ?>/public/assets/violete.jpg" alt="violete.jpg" class="avatar" />
     <h1>Login</h1>
-    <form action="<?php echo URL ?>/public/login/checkValidity" method="Post">
+    <div id="form">
       <p>Username</p>
-      <input type="text" placeholder="Enter Username" name="username" />
+      <input type="text" placeholder="Enter Username" name="username" id="username" />
       <p>Password</p>
-      <input type="password" placeholder="Enter Password" name="password" />
-      <input type="submit" value="Login" />
+      <input type="password" placeholder="Enter Password" name="password" id="password" />
+      <input type="submit" value="Login" id="submitbutton" />
       <a id="acc_create" href="<?php echo URL ?>/public/?url=register">Create an account!</a>
-    </form>
+    </div>
   </div>
+  <script>
+    const form = {
+      username: document.getElementById('username'),
+      password: document.getElementById('password'),
+      submit: document.getElementById('submitbutton')
+    };
+
+    form.submit.addEventListener('click', () => {
+      console.log('clicked');
+      const request = new XMLHttpRequest();
+
+
+      request.onload = () => {
+        let response = null;
+
+        try{
+          response = JSON.parse(request.responseText);
+        }catch (e){
+          console.error('could not parse JSON!');
+        }
+
+        if(response){
+          handleResponse(response);
+        }
+
+      };
+
+      const requestData = `username=${form.username.value}&password=${form.password.value}`;
+
+      request.open('post', '<?php echo URL ?>/public/login/checkValidity');
+      request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      request.send(requestData);
+    });
+
+    function handleResponse (response){
+      console.log(response);
+      if(response.ok) {
+        console.log("true");
+        location.href = 'http://localhost/mvc/public/?url=profile';
+      }
+      else console.log("false");
+    }
+  </script>
 </body>
 
 </html>
